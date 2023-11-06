ebda9abd68d7198eebab47604fb84f8b28624b48
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-11-06 09:02:47 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, {useEffect, useState} from 'react'
+
+function App() {
+    const [isTrue, setIsTrue] = useState(false)
+    const [dog, setDog] = useState('')
+
+    useEffect(() => {
+        
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(dogs => {
+            setIsTrue(true)
+            setDog(dogs.message)
+
+        })
+    }, [])
+
+  return (
+    <div>
+    {isTrue ? <img src={dog} alt='A Random Dog'></img> : <p>Loading...</p>}
+    </div>
+  )
+}
+
+
+
+export default App
